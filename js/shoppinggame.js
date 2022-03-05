00559918bc18654e2bb59981a9b6f181841e2759
willberg845 <willberg845@gmail.com>
2022-03-05 13:41:00 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -48,8 +48,8 @@ Object.defineProperty(Product.prototype, "daysToExpire", {
 
 // Add method getDetails to Product here
 
-Product.prototype.getDetails = function(){
-    return "Product Name: ${this.name} , Product Price: ${this.price}";
+Product.prototype.getDetails = function () {
+    return 'Product Name: ${this.name} , Product Price: ${this.price}';
 }
 
 // Define the MagicProduct class here
