f24609254f28b36fe7a6510ed299b1927ca8f97d
willberg845 <willberg845@gmail.com>
2022-03-05 15:28:42 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -279,7 +279,7 @@ const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
 
-    player.score += pointsToBill + pointsForExpDate;
+    player.score = player.score + pointsToBill + pointsForExpDate;
 
     if (prod.constructor === MagicProduct) {
         if (prod.isBonus) {
