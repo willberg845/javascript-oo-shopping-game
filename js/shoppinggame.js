4733456a3ef58e25e2d5dcdd50ff722f58d2eaf1
willberg845 <willberg845@gmail.com>
2022-03-05 15:37:05 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -304,6 +304,9 @@ function init(data) {
 
         rl.question("What's your name? ", function (name) {
             // Assign the player object's name property to the user entered name here
+
+            player.name = window.prompt("Please enter your name:");
+
             console.log(`Welcome ${player.name} !!!`.blue);
             start(data);
         });
