a50b07e86537180d7c0b32c468a51281afd14ac2
willberg845 <willberg845@gmail.com>
2022-03-05 15:05:52 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -231,7 +231,9 @@ const findProductById = (id) => {
 };
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random()*20) + 1;
+};
 
 
 const getProduct = (prodList, pId) => {
