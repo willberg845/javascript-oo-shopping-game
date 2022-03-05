5633e47529088d56a7f0f5229cd2387705d0da44
willberg845 <willberg845@gmail.com>
2022-03-05 16:19:17 -0500

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -380,14 +380,20 @@ function init(data) {
     // Complete this function
     const rateAndExit = () => {
         // Create a new instance of Rating and assign it to a variable named playerRating here
+        playerRating = new Rating();
+
         rl.question("How would you rate this game on a scale of 1-10 (1 being the lowest)?:", function (r) {
             if (r == "" || isNaN(r) || r == 0 || r > 10) {
                 console.log("Invalid rating! Please nter a number from 1 - 10".red);
                 rateAndExit();
             } else {
                 // Call rating setter method of playerRating to set user entered rate value here
+
+                playerRating.rating(r);
                 
                 // Call Object.assign() method here to populate target
+
+                //target = Object.assign("",)
                 
                 console.log(`${target.name} you rated this game as ${target.rate}`.green);
                 console.log("Thank you for your valuable feedback.".blue);
